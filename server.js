eef1b56fd8d6d33383bdba4f6f36f83e3ccdf3f7
George Matos <georgemattos@gmail.com>
2020-11-09 17:00:35 -0300

    criando model de produto

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require('express'); // gerenciamento das rotas REST da aplicacao
+const mongoose = require('mongoose'); // ORM Javascript
+const requireDir = require('require-dir'); // importa todos os arquivos de um diretorio
 
 // Iniciando o App
 const app = express();
@@ -8,16 +9,25 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/nodeapi', {
   useUnifiedTopology: true,
   useNewUrlParser: true 
-})
+});
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 /**
  * Primeira rota
- * Parametros
+ * Significado dos parametros
  * req: cabecalho, corpo, usuario, autenticacao, etc
  * res: resposta ao usuario
  */
 app.get('/', (req, res) => {
-  res.send("Hello Rocketseat")
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'http://github.com/facebook/react-native'
+  })
+  
+  return res.send("Hello Rocketseat")
 })
 
 app.listen(3001);
