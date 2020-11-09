230a9761505c4113d1c6eaabb14bdde7502689ba
George Matos <georgemattos@gmail.com>
2020-11-09 14:18:59 -0300

    instalacao do mongoose e conectando ao mongo

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o App
 const app = express();
+
+// Iniciando o BD
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true 
+})
+
 /**
+ * Primeira rota
  * Parametros
  * req: cabecalho, corpo, usuario, autenticacao, etc
  * res: resposta ao usuario
