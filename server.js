0e485835afc28a9c92a1f36f485d0be0923852ff
George Matos <georgemattos@gmail.com>
2020-11-09 08:42:10 -0300

    criando a primeira rota

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
 const app = express();
+/**
+ * Parametros
+ * req: cabecalho, corpo, usuario, autenticacao, etc
+ * res: resposta ao usuario
+ */
+app.get('/', (req, res) => {
+  res.send("Hello Rocketseat")
+})
 
 app.listen(3001);
