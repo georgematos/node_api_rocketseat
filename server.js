acd2c6d4eafc1f329b79f8e2147bc221814e6574
George Matos <georgemattos@gmail.com>
2020-11-13 08:23:14 -0300

    configurando o CORS middleware e adicionando arquivo com os produtos em formato json para backup

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
+
 app.use(express.json()) // permite enviar dados em formato JSON para a aplicacao
-app.use(cors());
+
+app.use((req, res, next) => { // Middleware CORS
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "*");
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", 'GET, PUT, POST, DELETE');
+  app.use(cors());
+  next();
+}
+
+);
 
 // Iniciando o BD (conexao com o banco de dados)
 mongoose.connect('mongodb://localhost:27017/nodeapi', {
